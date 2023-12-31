7da0d60d99d74b708708a514b842e22a0b8c001e
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-12-31 11:22:41 +0530

    Added the scripts for displaying the question and answer....

--- script.js
+++ script.js
@@ -35,9 +35,23 @@ const quizData = [
 
 const quiz = document.getElementById("quiz");
 const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.querySelector(".question");
+const questionEl = document.querySelector("#question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submit = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  const currentQuizData = quizData[currentQuiz];
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
